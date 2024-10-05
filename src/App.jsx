26b8a4d2cf8c210ef38ef255048858ae8202ff4d
Mathias <iwbsc2@gmail.com>
2024-10-05 08:23:19 +0200

    Set up basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import SideBar from "./components/SideBar";
+
 function App() {
   return (
     <>
-      <div>yo what</div>
+      <div
+        id="project-wrapper"
+        className="min-h-screen flex justify-center bg-sky-200"
+      >
+        <div
+          id="main-container"
+          className="p-4 w-[80%] bg-sky-300 shadow-lg rounded-lg flex flex-col gap-5 my-5"
+        >
+          <SideBar />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SideBar() {
+  return (
+    <div className="flex flex-col w-24 p-2 h-[100%] gap-10">
+      <p>ICON</p>
+      <p>Toggle darkmode</p>
+    </div>
+  );
+}
