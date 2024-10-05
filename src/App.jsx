bebed1d549702f6a371823a5ec2129ea332fd34a
Mathias <iwbsc2@gmail.com>
2024-10-05 09:03:28 +0200

    Started implementing Dark Mode functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import SideBar from "./components/SideBar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === "dark");
+      document.documentElement.classList.toggle("dark", savedTheme === "dark");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", !isDarkMode ? "dark" : "light");
+  };
+
   return (
     <>
       <div
         id="project-wrapper"
-        className="min-h-screen flex justify-center bg-sky-200"
+        className={`min-h-screen flex justify-center ${
+          isDarkMode ? "bg-slate-900" : "bg-gray-100"
+        }`}
       >
         <div
           id="main-container"
-          className="p-4 w-[80%] bg-sky-300 shadow-lg rounded-lg flex flex-col gap-5 my-5"
+          className="p-4 w-[80%] bg-sky-200 shadow-lg rounded-lg flex flex-col gap-5 my-5"
         >
-          <SideBar />
+          <SideBar toggleDarkMode={toggleDarkMode} />
         </div>
       </div>
     </>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function SideBar() {
+export default function SideBar(props) {
+  const { toggleDarkMode } = props;
+
   return (
-    <div className="flex flex-col w-24 p-2 h-[100%] gap-10">
-      <p>ICON</p>
-      <p>Toggle darkmode</p>
+    <div className="flex flex-col w-24 p-2 h-[100%] gap-10 bg-sky-300 rounded">
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
     </div>
   );
 }
