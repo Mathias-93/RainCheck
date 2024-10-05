47e6e009a8faa5c3d485529942dcf9b1b77607ad
Mathias <iwbsc2@gmail.com>
2024-10-05 11:43:32 +0200

    added icons to toggle switch and moved darkmode functionality to its own component

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function DarkMode(props) {
+  const { isDarkMode, toggleDarkMode } = props;
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <span className="mr-2 text-sm text-white">
+        {isDarkMode ? "Toggle Light Mode" : "Toggle Dark Mode"}
+      </span>
+      <div
+        onClick={toggleDarkMode}
+        className={`w-14 h-8 flex items-center bg-sky-100 dark:bg-slate-600 rounded-full p-1 cursor-pointer transition-colors duration-300 ${
+          isDarkMode ? "bg-blue-600" : "bg-gray-300"
+        }`}
+      >
+        <div
+          className={`bg-white dark:bg-gray-900 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 flex items-center justify-center ${
+            isDarkMode ? "translate-x-6" : "translate-x-0"
+          }`}
+        >
+          {isDarkMode ? (
+            <i className="fa-regular fa-moon text-yellow-300 flex items-center justify-center"></i>
+          ) : (
+            <i className="fa-solid fa-sun text-yellow-400 flex items-center justify-center"></i>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DarkMode from "./DarkMode";
 
 export default function SideBar(props) {
   const { toggleDarkMode, isDarkMode } = props;
@@ -8,23 +9,7 @@ export default function SideBar(props) {
       className="flex flex-col w-[150px] p-2 h-[100%] gap-10 rounded shadow-lg 
         dark:bg-slate-500 text-gray-200  bg-sky-300 transition-colors duration-300"
     >
-      <div className="flex flex-col items-center gap-2">
-        <span className="mr-2 text-sm text-white">
-          {isDarkMode ? "Toggle Light Mode" : "Toggle Dark Mode"}
-        </span>
-        <div
-          onClick={toggleDarkMode}
-          className={`w-14 h-8 flex items-center bg-gray-200 dark:bg-gray-600 rounded-full p-1 cursor-pointer transition-colors duration-300 ${
-            isDarkMode ? "bg-blue-600" : "bg-gray-300"
-          }`}
-        >
-          <div
-            className={`bg-white dark:bg-gray-900 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${
-              isDarkMode ? "translate-x-6" : "translate-x-0"
-            }`}
-          ></div>
-        </div>
-      </div>
+      <DarkMode toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
     </div>
   );
 }
